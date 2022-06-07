2e27e315ab3e973be94b728493286555862fde23
fedega15 <federico.galassi9@gmail.com>
2022-06-07 00:21:38 -0300

    push

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,31 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+let numeroDecimal = 0;
+
+for (let i = 0; i < num.length; i++) {
+  let posicion = num.length -1 -i;
+  (2 ** posicion) * num[i]
+  numeroDecimal = numeroDecimal + (2 ** posicion)* num[i]
+
+ 
+
+}
+
+
+
+return numeroDecimal;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var numeroBinario = [];
+  while(num / 2 !== 0) {
+    numeroBinario.unshift(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return numeroBinario.join("");
 
 }
 
